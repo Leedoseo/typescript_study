f5c83953a51f89b45a929576a84270387f54702a
Leedoseo <okok0840@naver.com>
2026-03-06 18:02:34 +0900

    any 타입과 unknown 타입

---
+++ section2/src/6. any&unknown.ts
@@ -0,0 +1,25 @@
+// any
+// 특정 변수의 타입을 확실히 모를 때 사용할 수 있는 타입
+let anyVar: any = 10; // 원래 10이 들어가서 number라는 타입으로 추론됐지만 :any로 타입을 지정해서 다른 모든 타입을 받을 수 있음
+anyVar = "hi"; 
+anyVar = true;
+
+let num: number = 10;
+num = anyVar; // 이건 가능
+// 단, any 타입을 쓰면 TypeScript가 가지는 이점을 모두 포기하는 것과 같음. -> 최대한 사용 X
+
+// unknown
+// 모든 타입을 저장할 수 있지만, 다른 타입에 unknown 타입을 넣을 순 없음
+let unknownVar: unknown;
+unknownVar = "";
+unknownVar = 1;
+unknownVar = () => {};
+
+// num = unknownVar 이건 안됨.
+
+if (typeof unknownVar === 'number') {
+    // unknownVar가 넘버 타입임을 확실히 밝혀주었을 때는 unknown타입의 변수를 number 타입으로 정제해서 사용할 수는 있음
+    // => 타입 정제라고 함.
+    num = unknownVar;
+}
+// any타입보단 unknown타입을 좀 더 사용함.
